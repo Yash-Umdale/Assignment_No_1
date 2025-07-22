e0ad28ba68ac0ea6a8ab1f7158e5625f84c8eee6
Yash-dot-coder <yashumdale123@gmail.com>
2025-07-22 23:26:08 +0530

    intit commit

---
+++ src/Ass1db.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ass1db {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[][] mat=new int[3][3];
+        int i,j;
+        System.out.println("Enter the 3x3 Matrix:-");
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                mat[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Orginal Matrix:-");
+        for(i=0;i<3;i++) {
+            for (j = 0; j < 3; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Transpose of the Matrix:-");
+        int [][] temp=new int[3][3];
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                temp[j][i]=mat[i][j];
+            }
+        }for(i=0;i<3;i++) {
+            for (j = 0; j < 3; j++) {
+                System.out.print(temp[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
